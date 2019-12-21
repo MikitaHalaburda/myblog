7cfb8dab4633cc313e56c75c0d32861737c3d3b2
mikita <mikitahalaburda@gmail.com>
2019-12-21 13:36:12 +0300

    [2] changed readme and split NavigationMenu code into two files

--- src/resources/containers/NavigationMenu/index.js
+++ src/resources/containers/NavigationMenu/index.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import Link from "react-router-dom/Link";
 
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
+import NavigationMenuComponent from "../../components/NavigationMenu";
 
 const menuItems = [
   { label: "Home", url: "/" },
@@ -24,18 +22,12 @@ const NavigationMenu = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const toggleMenu = event => setAnchorEl(event ? event.currentTarget : null);
   return (
-    <nav>
-      <Button onClick={toggleMenu} variant="contained" color="primary">
-        <MenuOutlinedIcon />
-      </Button>
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={() => toggleMenu(null)}
-      >
-        {menuItems.map(renderMenuItem(() => toggleMenu(null)))}
-      </Menu>
-    </nav>
+    <NavigationMenuComponent
+      toggleMenu={toggleMenu}
+      anchorEl={anchorEl}
+      menuItems={menuItems}
+      renderMenuItem={renderMenuItem}
+    />
   );
 };
 
