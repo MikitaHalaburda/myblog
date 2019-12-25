b5c95c5f382b91d06eb6a71acaf12bc49d67e7ba
mikita <mikitahalaburda@gmail.com>
2019-12-25 16:18:21 +0300

    Experiment with memo, useCallback

--- src/resources/components/NavigationMenu/index.js
+++ src/resources/components/NavigationMenu/index.js
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
 import Menu from "@material-ui/core/Menu";
 import { BaseButton } from "../../../shared-components/Buttons";
 
-/* 
-    TODO: 
-        1) What is about to wrap toggleMenu into useCallback
-        2) Does it make sense to use React.memo here?
-*/
 const NavigationMenuComponent = ({
   toggleMenu,
   anchorEl,
   menuItems,
   renderMenuItem
-}) => (
-  <nav>
-    <BaseButton onClick={toggleMenu} text={<MenuOutlinedIcon />} />
-    <Menu
-      anchorEl={anchorEl}
-      open={Boolean(anchorEl)}
-      onClose={() => toggleMenu(null)}
-    >
-      {menuItems.map(renderMenuItem(() => toggleMenu(null)))}
-    </Menu>
-  </nav>
-);
+}) => {
+  const handleToggleMenu = useCallback(toggleMenu, []);
+  const buttonText = useCallback(<MenuOutlinedIcon />, []);
+  return (
+    <nav>
+      <BaseButton onClick={handleToggleMenu} text={buttonText} />
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={() => toggleMenu(null)}
+      >
+        {menuItems.map(renderMenuItem(() => toggleMenu(null)))}
+      </Menu>
+    </nav>
+  );
+};
 
 export default NavigationMenuComponent;
